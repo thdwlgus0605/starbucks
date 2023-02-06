585ede55219f4aa0ce426cee9e29061d19027c82
thdwlgus0605 <thdwlgus0605@naver.com>
2023-02-07 00:20:28 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-const searchEl = document.querySelector('.search');
-// document대신 SearchEl 작성
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-    // search요소를 클릭하면 SearchInput 요소에 포커스
-    searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-    // searchInput요소에 focus가 되면 searchEl요소에 focused를 추가
-    searchEl.classList.add('focused');
-    // searchInput요소를 클릭하면 html요소를 추가하는 기능/문구나타나게함
-    searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-// 포커스가 해제됐을 때를 의미
-searchInputEl.addEventListener('blur', function () {
-    // searchInput요소에 focus 제거
-    searchEl.classList.remove('focused');
-    // searchInput요소를 클릭하면 html요소를 추가하는 기능/문구삭제
-    searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl =document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 // window는 결국 우리가 보고잇는 화면자체, 설치한 lodash라이브러리를 통해 해당 명령어 사용
@@ -167,7 +142,3 @@ function floatingObject(selector, delay, size) {
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
-
-const thisYear = document.querySelector('.this-year');
-// textContent는 그 요소가 가지고있는 글자내요을 알아내거나 지정하는 용도
-thisYear.textContent = new Date().getFullYear();
